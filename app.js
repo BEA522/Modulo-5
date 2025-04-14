f197ec879fe2173972d874e583bccb5b9aa98a67
Bea <bea.loforte@gmail.com>
2025-04-14 17:20:20 -0300

    Espacio-app

--- app.js
+++ app.js
@@ -35,9 +35,6 @@ pool.query('select * from empleados').then(function (resultados) {
 
 
 
-
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
